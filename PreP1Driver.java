328e4cd3a9bb256ecaf2357c7813d95c067d59d0
W Carr <carrwi96@students.rowan.edu>
2023-10-26 11:39:25 -0400

    fixed some formatting

--- PreP1Driver.java
+++ PreP1Driver.java
@@ -17,12 +17,15 @@ public class PreP1Driver {
 
     public static void main(String[] args) throws ArithmeticException, NumberFormatException, IOException {
 
-        System.out.println("\nEnter the number of disks for the Towers of Hanoi:\n");
+        System.out.print("\nEnter the number of disks for the Towers of Hanoi: ");
         int number = Integer.parseInt(stdin.readLine().trim());
         // System.out.println(number);
+
+        System.out.println("\n");
+
         int[] counters = {0, 0}; // first is calls to solve second is disk moves
         solve(number, "The Left Tower", "The Right Tower", "The Middle Tower", counters);
-
+        System.out.printf("%ncalls: %d%ndisk moves: %d%n", counters[0], counters[1]);
     }
 
     public static void solve(int n, String I, String D, String T, int[] counters) {
