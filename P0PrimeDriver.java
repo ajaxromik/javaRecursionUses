1b6a92e9a80ccba3e9efcdd3cdc2b2c2ceac6306
W Carr <carrwi96@students.rowan.edu>
2023-10-26 11:02:45 -0400

    wrote the basic p0prime

---
+++ P0PrimeDriver.java
@@ -0,0 +1,21 @@
+
+/*
+ * Purpose: Data Structure and Algorithms Lab 7 Problem 0'
+ * Status: Complete and thoroughly tested
+ * Last update: 10/25/23
+ * Submitted:  10/25/23
+ * Comment: test suite and sample run attached
+ * Comment: I declare that this is entirely my own work
+ * @author: William Carr
+ * @version: 2023.10.25
+ */
+
+public class P0PrimeDriver {
+
+    public static void main(String[] args) throws ArithmeticException, NumberFormatException {
+
+        long[] bigArray = new long[Integer.parseInt(args[0])];
+
+    }
+
+}
